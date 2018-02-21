70e9963852d7e6e3b049b1b70e1ee8e36ca6ba5c
serqol <serqol@mail.ru>
2018-02-21 20:31:16 +0300

    dsa

--- app.php
+++ app.php
@@ -2,3 +2,29 @@
 
 $rootDir = isset($argv[1]) && is_dir($argv[1]) ? $argv[1] : null;
 
+$rootDir = __DIR__ . '/test';
+
+function getFileExtension($fileName) {
+    return substr(strrchr($fileName, '.'), 1);
+}
+
+function getPhpClassesRecursive($dir) {
+    $result = [];
+    $contents = array_filter(scandir($dir), function ($content) {
+        return !in_array($content, ['.', '..']);
+    });
+    foreach ($contents as $content) {
+        if (is_dir($subDirectory = "{$dir}/{$content}")) {
+            $result[$subDirectory] = getPhpClassesRecursive($subDirectory);
+            continue;
+        }
+        if (getFileExtension($content) === 'php') {
+            $result[$dir] = $content;
+        }
+    }
+    return $result;
+}
+
+$result = getPhpClassesRecursive($rootDir);
+
+var_dump($result);
---
+++ test/SubDir/AnotherTestClass.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: serqol
+ * Date: 21.02.18
+ * Time: 0:23
+ */
+
+class AnotherTestClass {
+
+}
