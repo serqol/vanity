75b4e474cd1392027b1e574d0ac6861489c013d3
Alexey Loginov <a.loginov@outlook.com>
2018-02-22 14:12:56 +0300

    das

--- app.php
+++ app.php
@@ -1,11 +1,16 @@
 <?php
 /**
+* @author  <>
+*/
+/**
  * Usage php __FILE__ {directory} {authorName} {authorEmail}
  */
 
 $rootDir = isset($argv[1]) && is_dir($argv[1]) ? $argv[1] : null;
 $author  = isset($argv[2]) ? $argv[2] : null;
 $email   = isset($argv[3]) ? $argv[3] : null;
+
+$rootDir = __DIR__;
 
 $phpOpenTag = '<?php';
 
@@ -33,8 +38,8 @@ function getPhpClassesRecursive($dir) {
             $result = array_merge($result, getPhpClassesRecursive($subDirectory));
             continue;
         }
-        if (getFileExtension($content) === 'php' && (strpos(file_get_contents($dir . '/' . $content), $phpOpenTag) === 0)) {
-            $result[$dir] = $content;
+        if (getFileExtension($content) === 'php' && (strpos(file_get_contents($phpClass = $dir . '/' . $content), $phpOpenTag) === 0)) {
+            $result[] = $phpClass;
         }
     }
     return $result;
@@ -42,13 +47,12 @@ function getPhpClassesRecursive($dir) {
 
 function applyAnnotation(array $phpClasses, $annotation) {
     global $phpOpenTag;
-    foreach ($phpClasses as $path => $class) {
-        $file = $path . '/' . $class;
-        $contents = file_get_contents($file);
+    foreach ($phpClasses as $class) {
+        $contents = file_get_contents($class);
         $firstPart = substr($contents, 0, strlen($phpOpenTag));
         $lastPart = substr($contents, strlen($phpOpenTag));
         $contents = $firstPart . $annotation . $lastPart;
-        $fileStream = fopen($file, 'w');
+        $fileStream = fopen($class, 'w');
         fwrite($fileStream, $contents);
         fclose($fileStream);
     }
